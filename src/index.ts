af55c54dbb23b58b9025d35681f7a79786d84e14
ackselz <tanshaochong48@gmail.com>
2024-03-09 02:39:24 +0800

    feat: add express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import express from "express";
+
 console.log("hello world");
+
+const app = express();
+const PORT = 3000; // Or any port you prefer
+
+// GET /lookup?staffPassId=<id>
+app.get("/ping", async (_req, res) => {
+  res.status(200).send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
